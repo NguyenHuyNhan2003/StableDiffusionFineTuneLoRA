64bba347900e00a36b1314f22fe8610dc2ae5f53
NguyenHuyNhan2003 <nguyenhuynhan.dn@gmail.com>
2025-05-21 15:52:32 +0700

    message

--- train_text_to_image_lora_val.py
+++ train_text_to_image_lora_val.py
@@ -450,6 +450,8 @@ def main():
 
                 if noise_scheduler.config.prediction_type == "epsilon":
                     target = noise
+                elif noise_scheduler.config.prediction_type == "v_prediction":
+                    target = noise_scheduler.get_velocity(latents, noise, timesteps)
                 else:
                     raise ValueError(f"Unknown prediction type {noise_scheduler.config.prediction_type}")
 
